a46b6cae057fc0b7e029d73528479b26574dfc4d
NuralamMRH <nuralammrh2@gmail.com>
2026-03-11 01:19:01 +0600

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import HomePage from "./pages/HomePage";
-import OurModelPage from "./pages/OurModelPage";
-import BrandsPage from "./pages/BrandsPage";
-import WhyChinaPage from "./pages/WhyChinaPage";
-import ResourcesPage from "./pages/ResourcesPage";
-import ContactPage from "./pages/ContactPage";
-import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
-import TermsOfServicePage from "./pages/TermsOfServicePage";
 
 function App() {
   return (
